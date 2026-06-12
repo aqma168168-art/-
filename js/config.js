6d7448a56a3e3fcacb91cdec8c1cd0547aa69a96
aqma168168-art <aqma168168@gmail.com>
2026-06-12 22:57:01 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ============================================================
 const CK_CONFIG = {
   // ▼ 請換成你的 Apps Script Web App URL
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyhgIuHG4emEvjeU3ro6UVbHf5kmZ184SsrrjzOvfAc1ZAwiHXEma97UdPYc_h_lFBCaA/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxyeaOT9KotZ8XiPCCnnVCR4FgkrgB1aYcTag2bmAE9yCc4wAtjpPqqlycJQNKjMe5sLQ/exec',
   VERSION: '2.0.0',
   DEV_MODE: ['localhost','127.0.0.1'].includes(window.location.hostname),
 };
